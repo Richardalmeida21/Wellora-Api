fa669ac2f379d1b9be0336e657c1ac815d0be391
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-06-04 11:11:01 -0300

    Add files via upload

---
+++ wellora/src/main/java/br/com/fiap/wellora/WelloraApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wellora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelloraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelloraApplication.class, args);
+	}
+
+}
---
+++ wellora/src/test/java/br/com/fiap/wellora/WelloraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wellora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelloraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
